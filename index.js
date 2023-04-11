fb3764b64ebc8c4ecea75873c959f528443de2c9
HieA1lpha <engkauanrodrigues@gmail.com>
2023-04-10 23:45:19 -0300

    2023-04-10
    Comando que recebe o texto enviado pelo Terminal.

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-/*
-{
-    let y = process.openStdin()
-    y.addListener("data", res => {
-        let x = res.toString().trim().split(/ +/g)
-        bot.channels.cache.get("707689776265953340").send(x.join(" "));
-    });
+process.openStdin().addListener("data", res => {
+    let terminal = res.toString().trim();
 
-    //Falar com o terminal bot
-    let ym = process.openStdin()
-    y.addListener("data", res => {
-        let x = res.toString().trim().split(/ +/g)
-        let co = x[0];
-        let k = res.toString().trim().slice(co.length).split(/ +/g)
-        bot.channels.cache.get(x[0]).send(k.join(" "));
-    });
-}*/
+    let digitado = terminal.split(/ +/g);
+
+    console.log(digitado.join(" "));
+});
